31824222f9b3232d7cb1ca2920a14a5f76361f15
ChinDowns <webdev@chindowns.com>
2020-03-26 22:30:14 -0700

    Added formatting to Past housrs

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ function init() {
     // Populate the calendar with stored data from previous session between 8AM and 5PM (1700)
     for (i = 8; i < 18; i++) {
         var time = i + "00";
-            console.log("LocalStorage Time is: " + time);
+            // console.log("LocalStorage Time is: " + time);
         var timeData = localStorage.getItem(time);
         if (timeData !== null) {
-            console.log("LocalStorage Value for time: " + timeData);
+            // console.log("LocalStorage Value for time: " + timeData);
             document.getElementById(time).value = timeData;
         }
         var time = i + "30";
-            console.log("LocalStorage Time is: " + time);
+            // console.log("LocalStorage Time is: " + time);
         var timeData = localStorage.getItem(time);
         if (timeData !== null) {
-            console.log("LocalStorage Value for time: " + timeData);
+            // console.log("LocalStorage Value for time: " + timeData);
             document.getElementById(time).value = timeData;
         }
     }
@@ -65,4 +65,52 @@ $(document).ready(function () {
         localStorage.setItem(time, plannerInput);
 
     });
+
+    // Set the row colors for Past, Present, and Future
+    // get the current time and iterate through the table rows time
+    var trArr = [];
+    var str;
+    var num;
+
+    $("tbody tr").each(function() {
+        str = this.id;
+        // Use *1 to convert to a string because parseInt uses only whole numbers
+        num = str*1;
+        trArr.push(num);
+    });
+    var time = moment().format("H");
+
+    for (var i = 0; i < trArr.length; i++) {
+        if (trArr[i] < time){
+            var trEl = "#"+trArr[i];
+            $(trEl).addClass("past");
+        }
+    }
+
+
+    // var trEl = $("tr");
+    // var trArr = Array.from(trEl);
+    // console.log(trArr);
+
+    
+    // var trIdArr;
+    // for (var i = 1; i < trEl.length; i++) {
+        
+    //     trIdArr.push(trEl[i].id);
+
+    //     console.log(trIdArr);
+
+
+
+        // // console.log(realHour);
+        // var attr = $(`.${i}`);
+        // console.log(attr);
+        // // var hourClass = parseInt(attr[0].class);
+        // if (realHour < time) {
+        //     $(this).addClass("past");
+        //     console.log("trArr is less than time" + realHour);
+        // }
+    
+
+
 });
