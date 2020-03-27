ec14d9c40b4a0c6626f982c9f5989e1477133ef7
ChinDowns <webdev@chindowns.com>
2020-03-27 00:43:36 -0700

    Add Past, Present, Future but left commented out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Sets points to the table header to set the date.
 var elemTableDate = document.querySelector("#table-date");
 
 var now = new Date();
@@ -23,94 +24,79 @@ function init() {
     }
 }
 
-// Set the date in the table header
-var displayHeaderDate = function displayHead() {
-    var arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var arrDayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    var arrToday = String(now).split(" ", 5);
-    var month;
-    var dayOfWeek;
-    console.log(arrToday);
-    // Use for statement to interate through f the format of the date MMM DD YYYY
-    for (var i = 0; i < arrMonth.length; i++) {
-        if (arrMonth[i].includes(arrToday[1])) {
-            month = arrMonth[i];
-        }
-    }
-
-    for (var i = 0; i < arrDayOfWeek.length; i++) {
-        if (arrDayOfWeek[i].includes(arrToday[0])) {
-            dayOfWeek = arrDayOfWeek[i];
-            console.log("this is the day of the week: " + dayOfWeek);
-        }
-    }
-
-    var today = dayOfWeek + " " + month + " " + arrToday[2] + ", " + arrToday[3];
-    // elemTableDate.innerHTML = today;
-}
-
-displayHeaderDate();
 init();
 
+// Set the date in the table header
+// var displayHeaderDate = function displayHead() {
+//     var arrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     var arrDayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+//     var arrToday = String(now).split(" ", 5);
+//     var month;
+//     var dayOfWeek;
+//     console.log(arrToday);
+//     // Use for statement to interate through f the format of the date MMM DD YYYY
+//     for (var i = 0; i < arrMonth.length; i++) {
+//         if (arrMonth[i].includes(arrToday[1])) {
+//             month = arrMonth[i];
+//         }
+//     }
+
+//     for (var i = 0; i < arrDayOfWeek.length; i++) {
+//         if (arrDayOfWeek[i].includes(arrToday[0])) {
+//             dayOfWeek = arrDayOfWeek[i];
+//             console.log("this is the day of the week: " + dayOfWeek);
+//         }
+//     }
+
+//     var today = dayOfWeek + " " + month + " " + arrToday[2] + ", " + arrToday[3];
+// elemTableDate.innerHTML = today;
 
 $(document).ready(function () {
     // Display the today's in the header
     $("#table-date").text(moment().format("dddd, MMMM Do YYYY"));
 
+    // Listens for a chonge on the INPUT tags and logs it to LocalStorage
     $("input").on("change", function(event){
         event.preventDefault();
         var plannerInput = $(this).val();
         var time = $(this).attr("id");
 
         localStorage.setItem(time, plannerInput);
-
     });
 
-    // Set the row colors for Past, Present, and Future
-    // get the current time and iterate through the table rows time
-    var trArr = [];
-    var str;
-    var num;
+    // // Set the row colors for Past, Present, and Future
+    // // get the current time and iterate through the table rows time
 
-    $("tbody tr").each(function() {
-        str = this.id;
-        // Use *1 to convert to a string because parseInt uses only whole numbers
-        num = str*1;
-        trArr.push(num);
-    });
-    var time = moment().format("H");
+    // // Set the Table Row Array which will be used to iterate through the table ids,
+    // // and compare the table id to current time to set the color of the row. 
+    // var trArr = [];
 
-    for (var i = 0; i < trArr.length; i++) {
-        if (trArr[i] < time){
-            var trEl = "#"+trArr[i];
-            $(trEl).addClass("past");
-        }
-    }
+    // // Grabbing all the table rows in the table body which we will use to pull their id
+    // $("tbody tr").each(function() {
+    //     //Putting the table row ID into a string
+    //     var str = this.id;
+    //     // Use *1 to convert to a string because parseInt uses only whole numbers
+    //     // Need the string to be a number to compare to the hour.
+    //     var num = str*1;
+    //     trArr.push(num);
+    // });
 
-
-    // var trEl = $("tr");
-    // var trArr = Array.from(trEl);
-    // console.log(trArr);
-
+    // // get the hour and minutes
+    // var time = (moment().format("HHmm"));
     
-    // var trIdArr;
-    // for (var i = 1; i < trEl.length; i++) {
-        
-    //     trIdArr.push(trEl[i].id);
-
-    //     console.log(trIdArr);
-
-
-
-        // // console.log(realHour);
-        // var attr = $(`.${i}`);
-        // console.log(attr);
-        // // var hourClass = parseInt(attr[0].class);
-        // if (realHour < time) {
-        //     $(this).addClass("past");
-        //     console.log("trArr is less than time" + realHour);
-        // }
-    
-
-
+    // for (var i = 0; i < trArr.length; i++) {
+    //     console.log(trArr[i] + "  " + time);
+    //     console.log(trArr[i]+29);
+    //     if (trArr[i] < time){
+    //         var trEl = "#"+trArr[i];
+    //         $(trEl).attr("class", "past");
+    //     } else if ((trArr[i] <= time) && ((trArr[i]+29)>time)) {
+    //         var trEl = "#"+trArr[i];
+    //         $(trEl).attr("class", "present"); 
+    //     } else {
+    //         var trEl = "#"+trArr[i];
+    //         $(trEl).attr("class", "future"); 
+    //     }
+    // }
 });
+
